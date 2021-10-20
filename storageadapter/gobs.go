0064d8904687001458398ae04d256fbe6142cb48
Vadim Inshakov <vadiminshakov@gmail.com>
2021-10-20 16:11:26 +0500

    add rabbitmq msgs encode and decode methods
    Signed-off-by: Vadim Inshakov <vadiminshakov@gmail.com>

--- storageadapter/gobs.go
+++ storageadapter/gobs.go
@@ -9,6 +9,7 @@ import (
 	"bytes"
 	"encoding/gob"
 	"github.com/newity/crawler/parser"
+	"github.com/streadway/amqp"
 )
 
 func Encode(data *parser.Data) ([]byte, error) {
@@ -22,6 +23,25 @@ func Encode(data *parser.Data) ([]byte, error) {
 
 func Decode(data []byte) (*parser.Data, error) {
 	decoded := &parser.Data{}
+	bytebuffer := bytes.NewBuffer(data)
+	d := gob.NewDecoder(bytebuffer)
+	if err := d.Decode(&decoded); err != nil {
+		return nil, err
+	}
+	return decoded, nil
+}
+
+func EncodeRabbitMsg(data *amqp.Delivery) ([]byte, error) {
+	var bytebuffer bytes.Buffer
+	e := gob.NewEncoder(&bytebuffer)
+	if err := e.Encode(*data); err != nil {
+		return nil, err
+	}
+	return bytebuffer.Bytes(), nil
+}
+
+func DecodeRabbitMsg(data []byte) (*amqp.Delivery, error) {
+	decoded := &amqp.Delivery{}
 	bytebuffer := bytes.NewBuffer(data)
 	d := gob.NewDecoder(bytebuffer)
 	if err := d.Decode(&decoded); err != nil {
