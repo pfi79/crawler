e5af0acb9f2d269b2324e81feec2122f7241e8c0
Vadim Inshakov <vadiminshakov@gmail.com>
2021-11-04 13:10:49 +0500

    add EncodeBatch, DecodeBatch
    Signed-off-by: Vadim Inshakov <vadiminshakov@gmail.com>

--- storageadapter/gobs.go
+++ storageadapter/gobs.go
@@ -31,6 +31,25 @@ func Decode(data []byte) (*parser.Data, error) {
 	return decoded, nil
 }
 
+func EncodeBatch(data []parser.Data) ([]byte, error) {
+	var bytebuffer bytes.Buffer
+	e := gob.NewEncoder(&bytebuffer)
+	if err := e.Encode(data); err != nil {
+		return nil, err
+	}
+	return bytebuffer.Bytes(), nil
+}
+
+func DecodeBatch(data []byte) ([]parser.Data, error) {
+	var decoded []parser.Data
+	bytebuffer := bytes.NewBuffer(data)
+	d := gob.NewDecoder(bytebuffer)
+	if err := d.Decode(&decoded); err != nil {
+		return nil, err
+	}
+	return decoded, nil
+}
+
 func EncodeRabbitMsg(data *amqp.Delivery) ([]byte, error) {
 	var bytebuffer bytes.Buffer
 	e := gob.NewEncoder(&bytebuffer)
